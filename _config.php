e0b05fc490d9b5672d42e3fd8197474c1fb370f4
Duncan Rumbold <duncan@writethatessay.org>
2019-07-10 10:01:38 +1200

    FIX: Support PHP versions 7.2 and higher.

--- _config.php
+++ _config.php
@@ -1,5 +1,11 @@
 <?php
 
+// Ensure compatibility with PHP 7.2 ("object" is a reserved word),
+// with SilverStripe 3.6 (using Object) and SilverStripe 3.7 (using SS_Object)
+if (!class_exists('SS_Object')) {
+    class_alias('Object', 'SS_Object');
+}
+
 // // These power dev/health, which can be used by load balancers and other such systems
 // EnvironmentCheckSuite::register('health', 'DatabaseCheck');
 
